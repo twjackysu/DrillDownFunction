6b8be3ed2a334aac703ace2102d8022bf1a9a271
Jacky Su <jacky19918@gmail.com>
2023-03-18 17:14:31 +0800

    feat: add app settings and startup

---
+++ DrilldownFunctions/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrilldownFunctions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+
+            var config = builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configuration = builder.GetContext().Configuration;
+
+            builder.Services.AddOptions();
+            builder.Services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+
+        }
+    }
+}
